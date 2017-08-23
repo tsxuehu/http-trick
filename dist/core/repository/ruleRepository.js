75d01d85546b93704a9605c215eaa336994e9b6f
tsxuehu <tsxuehu@gmail.com>
2017-08-23 15:42:07 +0800

    init

---
+++ dist/core/repository/ruleRepository.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by tsxuehu on 8/3/17.
+ */
+class ruleRepository {
+    createRuleFile(userId) {}
+    getRuleFileList(userId) {}
+    getInuseRules(ip) {}
+}
+exports.default = ruleRepository;
