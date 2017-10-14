403dd7350d9005ebd4ae149bdc35e3d1e11efd2d
taiyong <taiyong@youzan.com>
2017-10-14 18:38:24 +0800

    init

--- src/core/uiServer/router.js
+++ src/core/uiServer/router.js
@@ -1,8 +1,8 @@
-import  Router from 'koa-router';
+import Router from "koa-router";
 export default function getRouter() {
     let router = new Router();
     // 注册中间件，获取用户身份
-    router.use(function *(next) {
+    router.use((ctx, next) => {
         // 取用户Id
 
         yield next;
