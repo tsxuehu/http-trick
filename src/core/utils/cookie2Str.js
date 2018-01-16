93abbbe73ceb8cad653609dc9e8a50524bda1c11
taiyong <taiyong@youzan.com>
2018-01-16 23:17:09 +0800

    重构

--- src/core/utils/cookie2Str.js
+++ src/core/utils/cookie2Str.js
@@ -3,7 +3,7 @@ const _ = require("lodash");
 module.exports = function (cookies) {
     let arr = [];
     _.forEach(cookies, (value, key) => {
-        arr.push(`${key}=${encodeURIComponent(value)}`)
+        arr.push(`${key}=${encodeURI(value)}`)
     });
 
     return arr.join("; ");
