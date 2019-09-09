c23d9085f51c87ea0b8d29445b2cd06608588425
taiyong <taiyong@youzan.com>
2019-09-09 13:16:49 +0800

    refactor

--- webui/src/manager/index.js
+++ webui/src/manager/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 // element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
--- webui/src/monitor/index.js
+++ webui/src/monitor/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
