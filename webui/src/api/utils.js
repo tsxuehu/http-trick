2aac86de468b5344760119cc68bef7765098874e
taiyong <taiyong@youzan.com>
2019-09-09 14:22:32 +0800

    删除monitor2之前

--- webui/src/api/utils.js
+++ webui/src/api/utils.js
@@ -6,20 +6,9 @@ import axios from 'axios';
 
 var api = {
 
-
     getRemoteFile(url) {
         return axios.get(`/utils/getRemoteFile?url=${encodeURIComponent(url)}`);
-    },
-
-    getGatewayMachine() {
-        return axios.get('/utils/gateway/get-machine');
-    },
-    getConfigInfo(deviceId) {
-        return axios.get(`/utils/gateway/config?deviceId=${deviceId}`);
-    },
-    setGateWay(deviceId, config){
-        return axios.post(`/utils/gateway/set?deviceId=${deviceId}`, config);
-    },
+    }
 };
 
 export default api;
